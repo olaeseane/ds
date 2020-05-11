187132d929e39ad1513977f6b22703d265de1225
olaesean <olaesean@gmail.com>
2020-05-11 08:58:36 +0300

    Module 0 - add predict func ver 1

---
+++ module_00/score_game.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def predict_number_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1,101)  # Предполагаемое число
+        if number == predict: 
+            return(count)   # Выход из цикла, если угадали
+
+
+def score_game(predict_func):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []   # Список для сохранения числа попыток угадывания
+
+    np.random.seed(1)   # Фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1))
+    
+    for number in random_array:
+        count_ls.append(predict_func(number))   # Угадывание числа и сохранение количества попыток угадывания
+    score = int(np.mean(count_ls)) 
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    
+    return(score)
+
+
+score_game(predict_number_v1)
